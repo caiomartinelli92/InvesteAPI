d298409ecf08fc19e88bc75ab8f945d51c6a7e4a
Caio Martinelli <caio.martinelli@gmail.com>
2024-03-19 22:57:54 -0300

    Adicionar arquivos de projeto.

---
+++ InvesteAPI/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace InvesteAPI
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 35 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
