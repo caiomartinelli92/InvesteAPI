fb80f51f4b279c6a5119a41b9177c3fc1f52b38a
Caio Martinelli <caio.martinelli@gmail.com>
2024-03-23 22:28:13 -0300

    Criação do modelo "Investimento".

---
+++ InvesteAPI/Models/Investimento.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvesteAPI.Models
+{
+    public class Investimento
+    {
+        [Key]
+        public int investimentoId { get; set; }
+
+        [Required]
+        public string? tipo { get; set; }
+
+        [Precision(8,2)]
+        public decimal aporteInicial { get; set; }
+
+        [Precision(8, 2)]
+        [Required]
+        public decimal aporteMensal { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "dd/mm/yyyy")]
+        public DateTime dataInicial { get; set; }
+
+        [Required]
+        public int tempoVigencia { get; set; }
+
+        [Precision(8, 2)]
+        public decimal valorFinal { get; set; }
+
+        public float CalculaEstimado(float aporteInicial, float aporteMensal, int vigencia, string tipo)
+        {
+            var juros = 0.15 / 12;
+            if (tipo == "Renda Fixa")
+            {
+                return (float)((aporteInicial + (aporteMensal * vigencia)) * juros * vigencia);
+            }
+            else
+            {
+                return (float)((aporteInicial + aporteMensal * Math.Pow((1 + juros), vigencia)));
+            }
+        }
+    }
+}
